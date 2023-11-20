6ae6772c386c425257336a4d37c9120746d1af23
warhio <matvey.i.zakharov@gmail.com>
2023-11-20 18:28:04 +0300

    second

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def good_function():
     print('good things')
 
 
+def say_truth():
+    print('vanya is true sigma gigachad... (not true)')
+
+
+def sigma():
+    print('ya sigma')
+
+
 if __name__ == '__main__':
     good_function()
-
 
